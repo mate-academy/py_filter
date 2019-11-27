14d9df972abbf880703cc502eecaadc42aa50661
DukhDmytro <cowboybebop4991@gmail.com>
2019-11-27 10:56:31 +0200

    1st commit

--- filter.py
+++ filter.py
@@ -1,6 +1,7 @@
+"""imports"""
 from typing import Callable, Iterable, Any, List
 
 
-def filter_(func: Callable[[Any], Any], l: Iterable[Any]) -> List[Any]:
-
-    return []
+def filter_(func: Callable[[Any], Any], lst: Iterable[Any]) -> List[Any]:
+    """filter"""
+    return [el for el in lst if func(el)]
