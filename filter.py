3f49267db5ce1c297a8a8a9707b261fb91de7a49
logotip <logotip123@yahoo.com>
2019-11-27 10:32:24 +0200

    v 1.0

--- filter.py
+++ filter.py
@@ -1,6 +1,12 @@
+"""Module with custom filter function"""
 from typing import Callable, Iterable, Any, List
 
 
-def filter_(func: Callable[[Any], Any], l: Iterable[Any]) -> List[Any]:
-
-    return []
+def filter_(func: Callable[[Any], Any], objects: Iterable[Any]) -> List[Any]:
+    """
+    custom filter function
+    :param func: function for filtration
+    :param objects: any iterable objects
+    :return: result list
+    """
+    return [x for x in objects if func(x)]
