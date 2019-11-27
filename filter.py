fd79e06886eaf2ac87ba120d3fa02be72ea932e5
Dima <dima.sapsay@gmail.com>
2019-11-27 11:55:24 +0200

    first commit

--- filter.py
+++ filter.py
@@ -1,6 +1,11 @@
+"""
+Create your own implementation of filter() function
+"""
+
+
 from typing import Callable, Iterable, Any, List
 
 
-def filter_(func: Callable[[Any], Any], l: Iterable[Any]) -> List[Any]:
-
-    return []
+def filter_(func: Callable[[Any], Any], lst: Iterable[Any]) -> List[Any]:
+    """my filter function"""
+    return [value for value in lst if func(value)]
