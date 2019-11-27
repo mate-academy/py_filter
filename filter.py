5f0fe5a71db4c7fd1eb7b4fd1aa3001028882a60
alexander <earthhuman41@gmail.com>
2019-11-27 08:55:38 +0200

    implemented filter

--- filter.py
+++ filter.py
@@ -1,6 +1,20 @@
+"""
+filter
+"""
 from typing import Callable, Iterable, Any, List
 
 
-def filter_(func: Callable[[Any], Any], l: Iterable[Any]) -> List[Any]:
+def filter_(
+        func: Callable[[Any], Any], list_to_filter: Iterable[Any]
+) -> List[Any]:
+    """
 
-    return []
+    :param func: callback
+    :param list_to_filter: list
+    :return: filtered list
+    """
+    result = []
+    for item in list_to_filter:
+        if func(item):
+            result.append(item)
+    return result
