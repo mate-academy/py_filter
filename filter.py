dbde676124acbaac065cf4b28de8babcfbde6d1d
vlysenok <tina.lysenok@gmail.com>
2019-11-27 12:54:26 +0200

    hw8_py_filter

--- filter.py
+++ filter.py
@@ -1,6 +1,18 @@
+"""
+module filter
+"""
 from typing import Callable, Iterable, Any, List
 
 
-def filter_(func: Callable[[Any], Any], l: Iterable[Any]) -> List[Any]:
-
-    return []
+def filter_(func: Callable[[Any], Any], lst: Iterable[Any]) -> List[Any]:
+    """
+    Implementation of filter() function.
+    :param func: Callable[[Any], Any]
+    :param lst: Iterable[Any]
+    :return: List[Any]
+    """
+    filter_result_lst = []
+    for element in lst:
+        if func(element) == element or func(element):
+            filter_result_lst.append(element)
+    return filter_result_lst
