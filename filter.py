c0655949cf618f230a4c840432a220d0c47daa6a
Andrii <andrewsotnikov88@gmail.com>
2019-11-30 19:23:31 +0200

    filter function solution

--- filter.py
+++ filter.py
@@ -1,6 +1,7 @@
+"""Create your own implementation of filter() function."""
 from typing import Callable, Iterable, Any, List
 
 
-def filter_(func: Callable[[Any], Any], l: Iterable[Any]) -> List[Any]:
-
-    return []
+def filter_(func: Callable[[Any], Any], array: Iterable[Any]) -> List[Any]:
+    """Filter function custom realization"""
+    return [elem for elem in array if func(elem) == elem or func(elem)]
