5fd62e6280a038576e41c138c004976cbd404109
koreyo <KoretsValentyn@gmail.com>
2019-12-01 22:50:18 +0200

    my filter

--- filter.py
+++ filter.py
@@ -1,6 +1,8 @@
+"""module"""
 from typing import Callable, Iterable, Any, List
 
 
-def filter_(func: Callable[[Any], Any], l: Iterable[Any]) -> List[Any]:
+def filter_(func: Callable[[Any], Any], itrbl: Iterable[Any]) -> List[Any]:
+    """filter"""
 
-    return []
+    return [i for i in itrbl if func(i)]
